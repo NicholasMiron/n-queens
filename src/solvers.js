490d3e57950840ea9c718069380c2d8b8f2967eb
Nicholas Miron <nicholasmiron@gmail.com>
2019-02-28 13:59:50 -0600

    Counted all rook solutions

--- src/solvers.js
+++ src/solvers.js
@@ -47,7 +47,47 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  //Variable of all passed solutions
+  var solutions = [];
+
+  //Create prefilled matrix;
+  var preStuffed = createMatrix(n);
+  for (let i = 0; i < preStuffed.length; i++) {
+    preStuffed[i][i] = 1;
+  }
+
+  //Create array of all possible combinations of 0 - n
+  var valsToN = new Array(n);
+  valsToN.forEach(function(element, index) {
+    element = index;
+  });
+  var permutations = findAllPermutations(valsToN);
+
+  //Fill board with match from prefilled and 
+  for (var permute = 0; permute < permutations.length; permute++) {
+    //create a custom matrix of prestuffed sorted in the order of permutations permute
+    var customMatrix = [];
+    for (var permi = 0; permi < permute.length; permi++) {
+      customMatrix[0] = preStuffed[permutations[permute[permi]]];
+    }
+
+    //Create new board
+    var customBoard = new Board(customMatrix);
+
+    if (!customBoard.hasAnyColConflicts() &&
+        !customBoard.hasAnyRowConflicts() &&
+        !customBoard.hasAnyMajorDiagonalConflicts() &&
+        !customBoard.hasAnyMinorDiagonalConflicts() ) {
+      var stringyCustom = JSON.stringify(customMatrix);
+      solutions.push(stringyCustom);
+    }
+
+  }
+
+
+
+
+  var solutionCount = solutions.length; //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -56,7 +96,6 @@ window.countNRooksSolutions = function(n) {
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = new Board(createMatrix(n)); //fixme
-
   for (let row = 0; row < n; row++ ) {
     for (let col = 0; col < n; col++) {
       var aRow = solution.get(row);
@@ -98,4 +137,39 @@ window.createMatrix = function(n) {
     outer.push(inner);
   }
   return outer;
+};
+
+window.findAllPermutations = function(anArr = []) {
+  var swap = function (array, pos1, pos2) {
+    var temp = array[pos1];
+    array[pos1] = array[pos2];
+    array[pos2] = temp;
+  };
+  
+  var heapsPermute = function (array, output, n) {
+    n = n || array.length; // set n default to array.length
+    if (n === 1) {
+      output(array);
+    } else {
+      for (var i = 1; i <= n; i += 1) {
+        heapsPermute(array, output, n - 1);
+        if (n % 2) {
+          var j = 1;
+        } else {
+          var j = i;
+        }
+        swap(array, j - 1, n - 1); // -1 to account for javascript zero-indexing
+      }
+    }
+  };
+  
+  var output = [];
+  
+  // For testing:
+  var print = function(input) {
+    output.push(input);
+  };
+  
+  heapsPermute(anArr, print);
+  return output;
 };
